ab1133c079c94d84180e0f29219d3681f58d5d63
danny aeng <noreference@web.de>
2019-11-29 13:20:38 +0100

    cpufreq: blu_schedutil: remove some oem bits
    this partially reverts commit d5fe2459cb3031101343622ec204613460a00101
    
    Signed-off-by: danny aeng <noreference@web.de>

--- kernel/sched/cpufreq_blu_schedutil.c
+++ kernel/sched/cpufreq_blu_schedutil.c
@@ -20,7 +20,6 @@
 #include <linux/sched/sysctl.h>
 #include "sched.h"
 #include "tune.h"
-#include <../drivers/oneplus/coretech/opchain/opchain_helper.h>
 
 /* Stub out fast switch routines present on mainline to reduce the backport
  * overhead. */
@@ -148,10 +147,8 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 		return;
 	}
 
-	if (policy->cur == next_freq) {
-		sg_policy->next_freq = next_freq;
+	if (sg_policy->next_freq == next_freq)
 		return;
-	}
 
 	sg_policy->next_freq = next_freq;
 	sg_policy->last_freq_update_time = time;
@@ -198,10 +195,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	unsigned int freq = arch_scale_freq_invariant() ?
 				policy->cpuinfo.max_freq : policy->cur;
 
-	if (!policy->cpu || !opc_fps_check(0))
-		freq = (freq + (freq >> 2)) * util / max;
-	else
-		freq = freq * util / max + 1;
+	freq = (freq + (freq >> 2)) * util / max;
 
 	if (freq == sg_policy->cached_raw_freq && sg_policy->next_freq != UINT_MAX)
 		return sg_policy->next_freq;
